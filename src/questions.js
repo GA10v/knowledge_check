aa70c7626ea6c2b53770e08f58a84bb5baea8547
Galovach Andrey <a.galovach@yandex.ru>
2022-02-16 10:16:20 +0300

    added questions

---
+++ src/questions.js
@@ -0,0 +1,447 @@
+/*
+    Python Core excercises
+*/
+var pythonCoreExcercisesArray = [{
+        "question": "Программа: Даны два числа. Нужно вывести их произведение, \
+                    если оно больше 1000. Если меньше - вывести их сумму.",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Есть последовательность чисел от 1 до 10. \
+                    Нужно последовательно вывести сумму текущего числа и предыдущего на итерации.\
+                    Пример: 1 + 2, 2 + 3 и т.д."
+    },
+    {
+        "question": "Программа: Дана строка. \
+                    Нужно удалить N первых символов в строке.",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Пользователь вводит строку. Нужно вывести все буквы, чей индекс в массиве нечетный\
+                    Пример: 'python' -> p, t, o",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Нужно определить совпадает ли первый и последний элементы в списках\
+                    Пример: [1, 4, 34, 1] -> True, [`asd`, `dsa`, `test`] -> False",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Требуется определить какие числа в списке делятся на 4\
+                    Пример: [1, 32, 14, 40] -> 32, 40"
+    },
+    {
+        "question": "Программа: Пользователь вводит строку. Нужно перевернуть эту сроку и вывести\
+                    Пример: This is sparta!!! -> !!!atrasp si sihT",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Нужно превратить список строк в одну одну строку",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Пользователь вводит строку. Пользователь вводит подстроку. \
+                        Нужно определить сколько раз подстрока встречается в исходной строке."
+    },
+    {
+        "question": "Программа: Дан список и два индекса. Нужно элементы по этим индексам поменять местами. \
+                        Пример: [1, 32, 14, 40]; 0; 2 -> [14, 32, 1, 40]"
+    },
+    {
+        "question": "Программа: Дана дата и время. Конвертируйте их во временную метку (Timestamp) \
+                    Пример: '2021-12-02 18:02:49' -> 1638468169",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Дан словарь Students c именами (ключ) и возрастом (значение). \
+                    Требуется отсортировать по именам (ключам) и вывести на экран в алфавитном порядке."
+    },
+    {
+        "question": "Программа: Продемострируйте работу replace, find, join, split в Python на строках.",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Продемострируйте работу slicing в Python на строках и коллекциях.",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Пользователь вводит число. Нужно вывести соответствующее римское число",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Отсортируйте следующий список по типам данных: \
+                     sourceList = [\"Volvo\", 25, \"BMW\", True, 100, -1, False, \"Kamaz\", True]"
+    },
+    {
+        "question": "Программа: Отсортируйте следующий список по алфавиту: \
+                     sourceList = [\"volvo\", \"VAZ\", \"BMW\", \"kamaz\", \"UAZ\", \"audi\"]",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Продемострируйте возможность сортировки в Python.",
+        "disabled": true
+
+    },
+    {
+        "question": "Программа: Нужно найти url в строке. \
+                    Пример: Please follow http://pypi.python.org/pypi?%3Aaction=index to learn PyPi -> http://pypi.python.org/pypi?%3Aaction=index",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Дан символ. Нужно распечатать его ASCII код.\
+                        Пример: The ASCII value of g is 103",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Нужно найти наибольший элемент в кортеже.\
+                        (-5, 18, -100, 55, 108, 12, 0) -> 108"
+    },
+    {
+        "question": "Программа: Продемострируйте работу с побитовыми операторами."
+    },
+    {
+        "question": "Программа: Выведите текущее время в Бангалоре (Индия)"
+    },
+    {
+        "question": "Программа: Пользователь вводит строку. Найти все дублирующиеся символы. \
+                    Пример: 'Andrey is the best in Python' -> n, e, y, i, s, t, h",
+        "disabled": true
+    },
+    {
+        "question": "Программа: Напишите программу, определяет является ли строка палиндромом. \
+                    Пример: 'Лёша на полке клопа нашёл' -> True"
+    },
+    {
+        "question": "Программа: Пользователь вводит слово. Нужно подсчитать сколько раз \
+                    встречается каждая буква в этом слове. Пример: 'Алладин' -> а=2, л=2, д=1, и=1, н=1"
+    }
+]
+
+/*
+    Python Core interview questions
+*/
+var pythonCoreArray = [{
+        "question": "Какие коллекции в Python вы знаете?"
+    },
+    {
+        "question": "Расскажите в чем разница между локальными и глобальными переменными? \
+                    Как можно обеспечить доступ к глобальной переменной из нескольких модулей?"
+    },
+    {
+        "question": "Для чего используется функция enumerate() в Python?",
+        "disabled": true
+    },
+    {
+        "question": "Какие вы знаете модули и методы для работы с временем/датами в Python?"
+    },
+    {
+        "question": "Как вы понимаете такую функциональность как List Comprehension?"
+    },
+    {
+        "question": "Какие вы знаете методы для работы со словарями (dict)?"
+    },
+    {
+        "question": "Как определить путь к модулю, существующие функции в модуле и получить справку по функциям в Python?"
+    },
+    {
+        "question": "Какой недостаток у кода: listNew = listSource? Перечислите какие вы знаете способы копирования списков (list)?"
+    },
+    {
+        "question": "Можно ли вернуть сразу несколько значений из функции в Python?"
+    },
+    {
+        "question": "Какие вы знаете способы объединения списков (list) в Python?"
+    },
+    {
+        "question": "Какой метод нужно вызвать, чтобы добавить новый элемент в множество (set)? В какую позицию попадет новый элемент?"
+    },
+    {
+        "question": "Какие вы знаете методы для работы со списками (list) и множествами (set)?"
+    },
+    {
+        "question": "Какие вы знаете методы для работы со строками (string)?"
+    },
+    {
+        "question": "Для чего нужна встроенная функция exec()?",
+        "disabled": true
+    },
+    {
+        "question": "Какие вы знаете методы для работы с массивами (array)?"
+    },
+    {
+        "question": "В чем разница между методами remove(), discard() и pop() при удалении элемента из множества (set)?"
+    },
+    {
+        "question": "Как из строки сделать список символов? Как из списка символов сделать строку?",
+        "disabled": true
+    },
+    {
+        "question": "Можете рассказать в каких реальных мировых программных продуктах используется Python?",
+        "disabled": true
+    },
+    {
+        "question": "В чем разница между range и xrange?",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите о способах объединения множеств (set), которые вы знаете."
+    },
+    {
+        "question": "Что вы можете рассказать о docstring?",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите про итераторы (iterators), генераторы (generators) и декораторы (decorators) \
+                        Для чего они используются и как устроены."
+    },
+    {
+        "question": "Что такое лямбда (lambda) в Python?"
+    },
+    {
+        "question": "Какие встроенные типы данных являются неизменяемые (immutable)?"
+    },
+    {
+        "question": "По ссылке или по значению передаются аргументы в функцию в Python?",
+        "disabled": true
+    },
+    {
+        "question": "Что вы можете рассказать про pickling and unpickling процессах",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите про модель управления памятью в Python",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите про преобразование типов (Python Casting)",
+        "disabled": true
+    },
+    {
+        "question": "В чем отличие динамической типизации от статической?",
+        "disabled": true
+    },
+    {
+        "question": "Какие виды операторов в Python вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "Что такое интерпретируемый язык программирования?",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите о PEP 8"
+    },
+    {
+        "question": "Расскажите что вы знаете о пространстве имен (namespace)? Для чего используется?"
+    },
+    {
+        "question": "Расскажите о PyPi"
+    },
+    {
+        "question": "Какие существуют области видимости в Python?",
+        "disabled": true
+    },
+    {
+        "question": "В чем предназначение отрицательного индекса в последовательностях?",
+        "disabled": true
+    },
+    {
+        "question": "В чем основные отличия между листом (list) и кортежем (tuple)?"
+    },
+    {
+        "question": "Какие встроенные типы данных в Python вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "Какие встроенные функции в Python вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "В чем разница между операциями \'==\' и \'is\'?"
+    },
+    {
+        "question": "В чем разница между set и frozenset?",
+        "disabled": true
+    },
+    {
+        "question": "Для чего используется инструкция pass в Python?"
+    },
+    {
+        "question": "В чем разница между deep copy и shallow copy?",
+        "disabled": true
+    },
+    {
+        "question": "Что вы можете рассказать о NumPy и SciPy?",
+        "disabled": true
+    },
+    {
+        "question": "Расскажите что вы знаете о slicing в Python?"
+    },
+    {
+        "question": "Для чего используется функция map() в Python?"
+    },
+    {
+        "question": "В чем состоит разница между массивами (arrays) и листами (lists)?"
+    },
+    {
+        "question": "Что такое модуль в Python? В чем состоит отличие от пакета?"
+    },
+    {
+        "question": "Расскажите о языке программирования Python. В чем заключается приемущество его использования? Почему вы его выбрали?"
+    },
+    {
+        "question": "Почему отступы в коде так важны в Python?",
+        "disabled": true
+    },
+    {
+        "question": "Какой версией Python вы пользуетесь? Расскажите какие отличия от предыдущей версии языка вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "Как оформляются комментарии в языке?"
+    }
+]
+
+/*
+    Python Multithreading interview questions
+*/
+var pythonMultithreadingArray = [{
+        "question": " В чем заключается разница между процессами и потоками?"
+    },
+    {
+        "question": "Какие существуют способы межпроцессорного взаимодействия?"
+    },
+    {
+        "question": "Как использовать модули Thread и Threading для создания потоков в Python?"
+    },
+    {
+        "question": "Какие есть способы синхронизации потоков в Python?"
+    },
+    {
+        "question": "Расскажите о модулях threading и multiprocessing в Python. В чем состоит отличие?"
+    },
+    {
+        "question": "Что такое многопоточность (multithreading). Перечислите плюсы и минусы многопоточности.",
+        "disabled": true
+    },
+    {
+        "question": "Что такое Python Global Interpreter Lock (GIL) и как он работает."
+    },
+    {
+        "question": "Что такое Thread?"
+    }
+];
+
+var sqlArray = [{
+        "question": "Как можно использовать оператор SELECT DISTINCT?"
+    },
+    {
+        "question": "Расскажите про сортировку результатов через SQL запросы?"
+    },
+    {
+        "question": "Что такое NULL значение в полях? Как в запросе проверить значение на NULL?"
+    },
+    {
+        "question": "Расскажите какие агрегированные функции в MySQL вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "Есть таблицы \'Customers\', \'Manufacturers\' и \'Countres\'. \
+        Нужно выбрать всех клиентов, которые находятся в тех же городах что и производители.",
+        "disabled": true
+    },
+    {
+        "question": "Есть таблица \'Products\' (ProductName, TypeId, Price). \
+        Нужно получить все товары только типа 1, 2 и 5 и у которых цена от 100$ до 150$.",
+        "disabled": true
+    },
+    {
+        "question": "Есть таблицы \'Customers\' (CustomerId, Name, Email), \'Orders\' (OrderId, CustomerId, OrderInfo) \
+        Нужно вывести все заказы с информацией о клиентах."
+    },
+    {
+        "question": "Есть таблицы \'Customers\' (CustomerId, Name, Email), \'Orders\' (OrderId, CustomerId, OrderInfo) \
+        Нужно вывести всех клиентов с заказами (NULL если нет заказа) и отсортировать клиентов по имени.",
+        "disabled": true
+    },
+    {
+        "question": "В чем разница между UNION и UNION ALL операторами?"
+    },
+    {
+        "question": "В чем разница между INNER JOINs и OUTER JOINs операторами?"
+    },
+    {
+        "question": "Есть таблицы \'Customers\' (CustomerId, Name, Country, City), \'Manufacturers\' (ManufacturerId, Name, Country, City) \
+        Нужно вывести все города России в алфавитном порядке, где расположены либо клиенты либо производители."
+    },
+    {
+        "question": "Есть таблицы \'Customers\' (CustomerId, Name, Country, City), \'Manufacturers\' (ManufacturerId, Name, Country, City) \
+        Нужно вывести всех клиентов и производителей с указанием типа Customer/Manufacturer."
+    },
+    {
+        "question": "Есть таблица \'Customers\' (CustomerId, Name, Country, City), \
+        Нужно вывести количество клиентов в каждой стране. Отсортировать по убыванию.",
+        "disabled": true
+    },
+    {
+        "question": "Для чего используется условие HAVING в sql запросах?",
+        "disabled": true
+    },
+    {
+        "question": "Есть таблица \'Customers\' (CustomerId, Name, Country, City), \
+        Нужно вывести количество клиентов в каждой стране только если их больше 3. Отсортировать по убыванию."
+    },
+    {
+        "question": "Какие ограничения (constraints) в MySQL вы знаете?",
+        "disabled": true
+    },
+    {
+        "question": "Что такое SQL UNIQUE Constraint? Как его добавить или удалить?"
+    },
+    {
+        "question": "Расскажите про PRIMARY KEY и FOREIGN KEY. Как его добавить или удалить?"
+    },
+    {
+        "question": "Расскажите в чем разница между CREATE INDEX и CREATE UNIQUE INDEX операторами? Как его добавить или удалить?",
+        "disabled": true
+    }
+
+];
+
+
+
+var generalOOPArray = [];
+
+var generalAlgorithmsArray = [];
+
+var generalDatabaseArray = [];
+
+var pythonAlgorithmsExcercisesArray = [];
+
+var pythonExceptionsArray = [];
+
+var pythonFileHandlingArray = [];
+
+var pythonClassesArray = [];
+
+var pythonClassesExcercisesArray = [];
+
+
+
+
+const ALL_TOPICS = {
+    'generalOOPArray': generalOOPArray,
+    'generalAlgorithmsArray': generalAlgorithmsArray,
+    'pythonCoreExcercisesArray': pythonCoreExcercisesArray,
+    'generalDatabaseArray': generalDatabaseArray,
+    'pythonCoreArray': pythonCoreArray,
+    'pythonMultithreadingArray': pythonMultithreadingArray,
+    'pythonAlgorithmsExcercisesArray': pythonAlgorithmsExcercisesArray,
+    'pythonExceptionsArray': pythonExceptionsArray,
+    'pythonFileHandlingArray': pythonFileHandlingArray,
+    'pythonClassesArray': pythonClassesArray,
+    'pythonClassesExcercisesArray': pythonClassesExcercisesArray,
+    'sqlArray': sqlArray
+
+
+};
